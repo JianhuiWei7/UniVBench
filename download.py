30e2a737c22bc07509a475de58d44c89d7c5441a
JianhuiWei7 <112548025+JianhuiWei7@users.noreply.github.com>
2026-03-23 17:13:37 +0800

    Update download.py

--- download.py
+++ download.py
@@ -1,15 +1,14 @@
 from huggingface_hub import snapshot_download
 
-local_save_path = "./downloaded_benchmark"
+local_save_path = "./UniVBench"
 
 print(f"Downloading dataset to {local_save_path} ...")
 
-# 下载整个数据集仓库的原始文件
 snapshot_download(
     repo_id="JianhuiWei/UniVBench",
     repo_type="dataset",
     local_dir=local_save_path,
-    local_dir_use_symlinks=False  # 设为 False 以确保下载的是实体文件而非软链接
+    local_dir_use_symlinks=False  
 )
 
 print("Dataset download completed!")
